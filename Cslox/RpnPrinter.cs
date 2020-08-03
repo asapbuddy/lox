b70e5d30cb56b3f0c63d513fae5c3d1646944664
Asap Buddy <asap.buddy@protonmail.com>
2020-08-03 16:22:06 +0300

    RPN: Just one test passed Need to fully implement or proof correctness

--- Cslox/RpnPrinter.cs
+++ Cslox/RpnPrinter.cs
@@ -1,27 +1,46 @@
-﻿namespace Cslox
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Cslox
 {
     public class RpnPrinter : Expr.IVisitor<string>
     {
+        private Stack<string> _stack = new Stack<string>();
+
         public string Print(Expr expr) => expr.accept(this);
 
         public string VisitBinaryExpr(Expr.Binary expr)
         {
-            throw new System.NotImplementedException();
+            return Parenthesize(expr.Oper.Lexem, expr.Left, new Expr.Literal(" "), expr.Right);
         }
 
         public string VisitGroupingExpr(Expr.Grouping expr)
         {
-            throw new System.NotImplementedException();
+            return expr.Expression.accept(this);
         }
 
         public string VisitLiteralExpr(Expr.Literal expr)
         {
-            throw new System.NotImplementedException();
+            return expr.value == null ? "nil" : expr.value.ToString();
         }
 
         public string VisitUnaryExpr(Expr.Unary expr)
         {
-            throw new System.NotImplementedException();
+            return $"{expr.Oper}";
+        }
+
+        private string Parenthesize(string name, params Expr[] exprs)
+        {
+            var result = "";
+            foreach (var expr in exprs)
+            {
+                result += $"{expr.accept(this)}";
+            }
+
+            result += $" {name}";
+
+
+            return result;
         }
     }
 }
